6436a38e3e7b89c62a72cfc08c46578ed597b4e3
lastorik <33267839+lastorik@users.noreply.github.com>
2017-10-31 23:41:53 +0200

    Add files via upload

---
+++ İsimsiz2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int sayi1;
+	
+	printf("sayiyi girin: ");
+	scanf("%d",&sayi1);
+	
+	if(sayi1%2==0)
+	printf("sayi çift");
+	else
+	printf("sayi tek");
+	
+	
+	getch();
+	
+}
