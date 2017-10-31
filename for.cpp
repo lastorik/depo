082ad27d09932aafd18fcddf8781c1683182c042
lastorik <33267839+lastorik@users.noreply.github.com>
2017-10-31 23:40:41 +0200

    Add files via upload
    for döngüsü ile ilgili örnek

---
+++ for.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include<locale.h>
+main()
+{
+	int toplam=0;
+	for(int i = 1; i < 11; i++)
+		toplam +=i;
+	printf("toplam = %d",toplam);
+	
+	
+	
+	getch();
+}
+
